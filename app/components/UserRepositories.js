8564e1f215df0945e0503a035dbfa8675ab4fb87
ga-wolf <jack.jeffress@ga.co>
2017-06-07 21:26:54 +1000

    Added sorting

--- app/components/UserRepositories.js
+++ app/components/UserRepositories.js
@@ -4,15 +4,19 @@ import Loading from "./Loading";
 class UserRepositories extends Component {
   render() {
     if (this.props.repos) {
-      const repos = this.props.repos.map(repo => {
-        return (
-          <li key={repo.id}>
-            <a href={repo.html_url} target="_blank">
-              {repo.name}
-            </a>
-          </li>
-        );
-      });
+      const repos = this.props.repos
+        .sort((a, b) => {
+          return a.name - b.name;
+        })
+        .map(repo => {
+          return (
+            <li key={repo.id}>
+              <a href={repo.html_url} target="_blank">
+                {repo.name}
+              </a>
+            </li>
+          );
+        });
       return (
         <div className="userRepo">
           <h5>Repositories</h5>
